4a79379e7c815115210ea1c580f3f3227dcb85ec
sititou70 <b1015208@fun.ac.jp>
2017-10-25 06:30:55 +0000

    [add]streaming ok
    but there are 8 sec ratency...

--- portforwarding.sh
+++ portforwarding.sh
@@ -2,6 +2,6 @@ self_port=$(cat ~/supsnap-camera-server/camera/config.py | grep SELF_ADDRESS | a
 
 while true; do
 	echo "$(date) start portforwarding..." >> ~/error.log
-	ssh -R 192.168.255.254:$self_port:localhost:5000 portforward@35.200.2.51 -N
+	ssh -oStrictHostKeyChecking=no -R 192.168.255.254:$self_port:localhost:5000 portforward@35.200.2.51 -N
 	sleep 1
 done
